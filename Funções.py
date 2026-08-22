9a234f0d7572d19a2db304de787a04f33a1f5c0f
17261562-boop <diogom.silva61@gmail.com>
2026-08-21 22:09:07 -0300

    Função tipo de carga e curva
    Adicionei as funções Curva de carga, uma função genérica para receber dados, e melhorei a função tipo_de_carca

--- Funções.py
+++ Funções.py
@@ -1,19 +1,45 @@
 def linha():
-    return print('-'*30)
+    print('-'*30)
+
 def tipo_de_carga():
-    print('Qual o tipo de Carga?')
-    linha
-    print('''1 Resistivo \n 2 Indutivo \n 3 Iluminação''')
+    print('Qual o tipo de Carga? \n')
+    print(''' 1) Resistiva \n 2) Indutiva \n 3) Iluminação \n''')
+    while True:
+        while True:
+            try:
+                carga=int(input('Digite o número: '))
+                break
+            except ValueError:
+                print('Digite o número correspondente a carga')
+    
+        if carga == 1:
+            carga = 'Resistiva'
+            break
+        elif carga == 2:
+            carga = 'Indutiva'
+            break   
+        elif carga == 3:
+            carga = 'Iluminação'
+            break
+        else:
+            print('Escolha uma das opções existentes')
+    return carga
+
+def curva_da_carga(carga):
+    if carga in ['Iluminação','Resistiva']:
+        curvatura = 'Curva B'
+    elif carga == 'Indutiva':
+        curvatura = 'Curva C'
+    return curvatura
+
+def receber_informacao(info):
     while True:
         try:
-            carga=int(input('Digite o número: '))
+            informacao = int(input(f'Insira a {info}: '))
             break
         except ValueError:
-            print('Digite o número correspondente a carga')
-        
-    if carga == 1:
-        carga = 'Resistivo'
-    elif carga == 2:
-        carga = 'Indutivo'
-    elif carga == 3:
-        carga = 'Iluminação'
+            print('Informe apenas números, sem "."')
+    return informacao
+
+def calculo_dtm(p,i,u):
+    
